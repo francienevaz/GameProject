b43420526e5f4d2d92a02a4a9353c9af688c81ca
francienevaz <fran.vfraga@gmail.com>
2022-04-30 12:14:49 -0300

    updated

--- JS/index.js
+++ JS/index.js
@@ -6,5 +6,29 @@ function start() { // Inicio da fun��o start()
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2' ></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+	//Principais vari�veis do jogo
+	
+	var jogo = {}
+	
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	
+	} // Fim da fun��o loop()
+
+
+
+//Fun��o que movimenta o fundo do jogo
+	
+	function movefundo() {
+	
+	esquerda = parseInt($("#fundoGame").css("background-position"));
+	$("#fundoGame").css("background-position",esquerda-1);
+	
+	} // fim da fun��o movefundo()
 
 }
